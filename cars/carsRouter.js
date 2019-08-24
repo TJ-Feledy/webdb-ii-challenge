6e3af50bab6c3cfea7371cdbea8e23c953e288c8
teej387 <teej387@yahoo.com>
2019-08-24 17:52:58 -0500

    Created get handler for api/cars/:id

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -13,3 +13,21 @@ router.get('/', (req, res) => {
       res.status(500).json({ errorMessage: 'Failed to retrieve cars' })
     })
 })
+
+router.get('/:id', (req, res) => {
+  const {id} = req.params
+
+  db('cars').where({id}).first()
+    .then(car => {
+      if (car) {
+        res.json(car)
+      }else {
+        res.status(404).json({ message: 'Invalid car ID' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: 'Failed to retrieve the car' })
+    })
+})
+
+module.exports = router
